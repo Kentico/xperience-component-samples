9593857e949d04e0f034acbc4df3876725137926
Peter Petrinec <PeterP@kentico.com>
2019-08-02 13:21:21 +0200

    MAE-65 Destroy Froala editor when destroing rich text inline editor

--- Kentico.InlineEditor.RichText/Assets/InlineEditors/RichText/main.ts
+++ Kentico.InlineEditor.RichText/Assets/InlineEditors/RichText/main.ts
@@ -3,11 +3,13 @@
 import FroalaEditor from "froala-editor/js/froala_editor.pkgd.min";
 import "froala-editor/css/froala_editor.pkgd.css";
 
+const RICH_TEXT_WRAPPER_SELECTOR = ".ktc-rich-text-wrapper";
+
 window.kentico.pageBuilder.registerInlineEditor("Kentico.InlineEditor.RichText", {
     init({ editor, propertyName }) {
-        const richTextWrapper = editor.querySelector(".ktc-rich-text-wrapper");
+        const richTextWrapper = editor.querySelector(RICH_TEXT_WRAPPER_SELECTOR);
 
-        const froala = new FroalaEditor(richTextWrapper, {
+        new FroalaEditor(richTextWrapper, {
             events: {
                 contentChanged() {
                     const event = new CustomEvent("updateProperty", {
@@ -17,10 +19,19 @@ window.kentico.pageBuilder.registerInlineEditor("Kentico.InlineEditor.RichText",
                             refreshMarkup: false
                         }
                     });
-                    
+
                     editor.dispatchEvent(event);
                 },
             },
         });
     },
+    destroy({ editor }) {
+        const richTextWrapper = editor.querySelector<any>(RICH_TEXT_WRAPPER_SELECTOR);
+        const froala = richTextWrapper["data-froala.editor"];
+        
+        if (froala) {
+            // Destroy Froala editor when destroying inline editor
+            froala.destroy();
+        }
+    }
 });
