d51671f09049294f34dd6c7e91fd9343ccf03738
Peter Petrinec <PeterP@kentico.com>
2019-08-28 15:09:59 +0200

    MAE-82 Fix dynamic text popup positioning

--- Kentico.InlineEditor.RichText/Assets/InlineEditors/RichText/plugins/macros/index.ts
+++ Kentico.InlineEditor.RichText/Assets/InlineEditors/RichText/plugins/macros/index.ts
@@ -11,9 +11,9 @@ const kenticoMacroPlugin = (editor: FroalaEditor): F.CustomPlugin => {
 
         // Create the list of buttons.
         if (editor.opts.popupButtons.length > 1) {
-            popup_buttons += '<div class="fr-buttons">';
+            popup_buttons += "<div class=\"fr-buttons\">";
             popup_buttons += editor.button.buildList(editor.opts.popupButtons);
-            popup_buttons += '</div>';
+            popup_buttons += "</div>";
         }
 
         // Load popup template.
@@ -22,7 +22,7 @@ const kenticoMacroPlugin = (editor: FroalaEditor): F.CustomPlugin => {
         };
 
         // Create popup.
-        var $popup = editor.popups.create('kenticoMacroPlugin.popup', template);
+        var $popup = editor.popups.create("kenticoMacroPlugin.popup", template);
 
         return $popup;
     }
@@ -30,31 +30,33 @@ const kenticoMacroPlugin = (editor: FroalaEditor): F.CustomPlugin => {
     // Show the popup
     function showPopup(macroEl: HTMLElement) {
         // Get the popup object defined above.
-        var $popup = editor.popups.get('kenticoMacroPlugin.popup');
+        let $popup = editor.popups.get("kenticoMacroPlugin.popup");
 
         // If popup doesn't exist then create it.
         // To improve performance it is best to create the popup when it is first needed
         // and not when the editor is initialized.
-        if (!$popup) $popup = initPopup();
+        if (!$popup) {
+            $popup = initPopup();
+        }
 
         // Set the editor toolbar as the popup's container.
-        editor.popups.setContainer('kenticoMacroPlugin.popup', editor.$oel);
+        editor.popups.setContainer("kenticoMacroPlugin.popup", editor.$oel);
 
         // Compute the popup's position.
         const { top, left } = macroEl.getBoundingClientRect();
-        var offsetLeft = left + macroEl.offsetWidth / 2;
-        var offsetTop = top;// + macroEl.offsetHeight;
+        const offsetLeft = left + macroEl.offsetWidth / 2;
+        const offsetTop = top + window.pageYOffset;
 
         // Show the custom popup.
         // The button's outerHeight is required in case the popup needs to be displayed above it.
-        editor.popups.show('kenticoMacroPlugin.popup', offsetLeft, offsetTop, macroEl.offsetHeight);
+        editor.popups.show("kenticoMacroPlugin.popup", offsetLeft, offsetTop, macroEl.offsetHeight);
 
         macroEl.classList.add("ktc-macro-active");
     }
 
     // Hide the custom popup.
     function hidePopup() {
-        editor.popups.hide('kenticoMacroPlugin.popup');
+        editor.popups.hide("kenticoMacroPlugin.popup");
 
         const activeMacro = editor.el.querySelector(".ktc-macro-active");
         if (activeMacro) {
@@ -72,24 +74,24 @@ const kenticoMacroPlugin = (editor: FroalaEditor): F.CustomPlugin => {
 export const initializeMacroPlugin = (froala: Froala) => {
     // Define popup template.
     Object.assign(froala.POPUP_TEMPLATES, {
-        'kenticoMacroPlugin.popup': '[_BUTTONS_]'
+        "kenticoMacroPlugin.popup": "[_BUTTONS_]"
     });
 
     // Define popup buttons.
     Object.assign(F.DEFAULTS, {
-        popupButtons: ['removeMacro', 'configureMacro'],
+        popupButtons: ["removeMacro", "configureMacro"],
     });
 
-    froala.DefineIcon('insertMacro', {
-        NAME: 'plus',
-        SVG_KEY: 'add'
+    froala.DefineIcon("insertMacro", {
+        NAME: "plus",
+        SVG_KEY: "add"
     });
     froala.RegisterCommand("insertMacro", insertMacroCommand);
     froala.RegisterCommand("removeMacro", removeMacroCommand);
 
-    froala.DefineIcon('configureMacro', {
-        NAME: 'bullhorn',
-        SVG_KEY: 'cogs'
+    froala.DefineIcon("configureMacro", {
+        NAME: "bullhorn",
+        SVG_KEY: "cogs"
     });
     froala.RegisterCommand("configureMacro", configureMacroCommand);
 
