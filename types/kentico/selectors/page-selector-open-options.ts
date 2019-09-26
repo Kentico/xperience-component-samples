a30dcff429bf215498d3726264f28d18690eba5d
Peter Petrinec <PeterP@kentico.com>
2019-09-26 16:47:59 +0200

    MAE-106 Add forgoten type files

---
+++ types/kentico/selectors/page-selector-open-options.ts
@@ -0,0 +1,9 @@
+import { Page } from "./page";
+
+export interface PageSelectorOpenOptions {
+    /**
+     * A callback function invoked when the page selector's confirmation button is clicked.
+     * @param selectedPages Selected pages.
+     */
+    readonly applyCallback: (selectedPages: Page[] | null) => void;
+}
