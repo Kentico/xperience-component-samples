94b386d37ee309b64c62290640291f198d94d72a
OndraM-Kentico <ondrejm@kentico.com>
2019-12-16 14:37:04 +0100

    MAE-77 Script code polishing

--- .nuget/CreatePackages.ps1
+++ .nuget/CreatePackages.ps1
@@ -2,23 +2,18 @@ Function PackRichText {
     Param (
         [Parameter(Mandatory=$true)]
         [string]
-        $version,
-        [Parameter(Mandatory=$true)]
-        [string]
         $configuration
     )
 
-    Write-Host "Get version: $version"
-    $suffixParam = "-Suffix b$env:APPVEYOR_BUILD_NUMBER"
-
     If ($env:APPVEYOR_REPO_TAG -eq "true") {
-        $suffixParam = ""
         $version = $env:APPVEYOR_REPO_TAG_NAME
-        Write-Host "APPVEYOR_REPO_TAG assumed to be $true"
-    } 
+    } else {
+        $referenceAssemblyPath = ".\Kentico.Widget.RichText\bin\$configuration\Kentico.Widget.RichText.dll"
+        $resolvedAssemblyPath = Resolve-Path -Path $referenceAssemblyPath
+        $version = DetermineAssemblyVersion $resolvedAssemblyPath
+        $suffixParam = "-Suffix b$env:APPVEYOR_BUILD_NUMBER"
+    }
 
-    Write-Host "Version: $version"
-    Write-Host "Suffix: $suffixParam"
     Invoke-Expression "nuget.exe pack .nuget\Kentico.EMS12.MvcComponents.Widget.RichText.nuspec -BasePath .\ -Version $version $suffixParam -Properties Configuration=$configuration"
 }
 
@@ -45,9 +40,5 @@ Function EnsureConfiguration {
 }
 
 $configuration = EnsureConfiguration
-$referenceAssemblyPath = ".\Kentico.Widget.RichText\bin\$configuration\Kentico.Widget.RichText.dll"
-$resolvedAssemblyPath = Resolve-Path -Path $referenceAssemblyPath
-$version = DetermineAssemblyVersion $resolvedAssemblyPath
 
-
-PackRichText $version $configuration
+PackRichText $configuration
