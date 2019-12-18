588735b68abc8d1eb76e9687e2e9dfdfdaf51da6
OndraM-Kentico <ondrejm@kentico.com>
2019-12-18 20:41:29 +0100

    MAE-77 Use env variable for version determination

--- .nuget/CreatePackages.ps1
+++ .nuget/CreatePackages.ps1
@@ -8,25 +8,16 @@ Function PackRichText {
     If ($env:APPVEYOR_REPO_TAG -eq "true") {
         $version = $env:APPVEYOR_REPO_TAG_NAME
     } else {
-        $referenceAssemblyPath = ".\Kentico.Widget.RichText\bin\$configuration\Kentico.Widget.RichText.dll"
-        $resolvedAssemblyPath = Resolve-Path -Path $referenceAssemblyPath
-        $version = DetermineAssemblyVersion $resolvedAssemblyPath
+        $version = GetVersion
         $suffixParam = "-Suffix b$env:APPVEYOR_BUILD_NUMBER"
     }
 
     Invoke-Expression "nuget.exe pack .nuget\Kentico.EMS12.MvcComponents.Widget.RichText.nuspec -BasePath .\ -Version $version $suffixParam -Properties Configuration=$configuration"
 }
 
-Function DetermineAssemblyVersion {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]
-        $assemblyPath
-    )
-
-    $version = [System.Reflection.Assembly]::LoadFile($assemblyPath).GetName().Version
-    $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-    $versionStr
+Function GetVersion {
+    $version = $env:APPVEYOR_BUILD_VERSION
+    $version.SubString(0, $version.LastIndexOf('.'))
 }
 
 Function EnsureConfiguration {
