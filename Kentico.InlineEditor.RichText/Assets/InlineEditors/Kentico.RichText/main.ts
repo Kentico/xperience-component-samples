116de19d33be0fa97f50ca50498f901624b970fc
Peter Petrinec <PeterP@kentico.com>
2020-01-31 13:39:46 +0100

    MAE-235 Pass correct FroalaEditor object when initializing custom plugins

--- Kentico.InlineEditor.RichText/Assets/InlineEditors/Kentico.RichText/main.ts
+++ Kentico.InlineEditor.RichText/Assets/InlineEditors/Kentico.RichText/main.ts
@@ -1,13 +1,13 @@
 ﻿/// <reference path="../../../../types/global.d.ts" />
 
-import * as Froala from "froala-editor/js/froala_editor.pkgd.min";
+import FroalaEditor from "froala-editor/js/froala_editor.pkgd.min";
 
 import { initializeFroalaEditor, destroyFroalaEditor } from "./froala";
 
 // Initialize custom plugins
 window.addEventListener("DOMContentLoaded", () => {
     window.kentico.pageBuilder.richTextEditor?.plugins?.forEach((customPlugin) => {
-        customPlugin(Froala);
+        customPlugin(FroalaEditor);
     });
 });
 
