c63bc0c7ba6b552d37f1af2dba4968ddc1897e77
Peter Petrinec <PeterP@kentico.com>
2020-04-28 16:47:57 +0200

    MAE-305 Add styles to object selector

--- Kentico.Selector.ObjectSelector/Assets/FormComponents/Kentico.ObjectSelector/main.ts
+++ Kentico.Selector.ObjectSelector/Assets/FormComponents/Kentico.ObjectSelector/main.ts
@@ -5,19 +5,25 @@ import "select2/dist/css/select2.css";
 import { ObjectSelectorItemModel } from "./types";
 import "./styles.less";
 
-document.addEventListener('initObjectSelector', () => {
-    const $formControl = $(".ktc-object-selector").empty();
-    $formControl.select2({
-        placeholder: "Search for item", //TODO MAE-305: Localize placeholder
-        minimumResultsForSearch: 50, //TODO MAE-305: Minimum results for search?
-        // minimumInputLength: 1, //TODO MAE-305: Minimum input length?
+const OBJECT_TYPE_ATTRIBUTE = "data-object-type";
+const GET_OBJECTS_ENDPOINT_URL_ATTRIBUTE = "data-get-objects-endpoint-url";
+const INITIALIZATION_EVENT_NAME = "Kentico.Selector.ObjectSelector.Initialize";
+
+document.addEventListener(INITIALIZATION_EVENT_NAME, (event) => {
+    const $selector = $(event.target as HTMLSelectElement).empty();
+
+    $selector.select2({
+        placeholder: "(None)", //TODO MAE-305: Represent as value not placeholder
+        // minimumResultsForSearch: 50, //TODO MAE-305: Minimum results for search?
+        // selectionCssClass: "ktc-object-selector-container",  //TODO MAE-305: Use this when https://github.com/select2/select2/issues/5843 is resolved
+        containerCssClass: "ktc-object-selector-container",
         dropdownCssClass: "ktc-object-selector-dropdown",
         ajax: {
-            url: $formControl.data("objects-endpoint"),
+            url: $selector.attr(GET_OBJECTS_ENDPOINT_URL_ATTRIBUTE),
             dataType: 'json',
             delay: 250,
             data: (params) => ({
-                objectType: $formControl.data("object-type"),
+                objectType: $selector.attr(OBJECT_TYPE_ATTRIBUTE),
                 page: params.page || 1,
                 searchTerm: params.term
             }),
@@ -33,4 +39,12 @@ document.addEventListener('initObjectSelector', () => {
             cache: true
         },
     });
+
+    // Ensure search input placeholder
+    $selector.one("select2:open", (event) => {
+        const { $dropdown } = $(event.target).data('select2');
+        $dropdown.find("input.select2-search__field").prop("placeholder", getString("SearchPlaceholder"));
+    });
 });
+
+const getString = (resourceString: string) => window.kentico.localization.strings[`Kentico.Selector.ObjectSelector.${resourceString}`];
