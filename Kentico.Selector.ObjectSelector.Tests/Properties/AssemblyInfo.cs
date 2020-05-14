f28f4efbdec7f0f114945a0e956bf381a5fd51e8
Peter Petrinec <petrinecp@users.noreply.github.com>
2020-05-14 14:10:45 +0200

    MAE-164 Ajax object selector implementation (#123)
    * MAE-303 Change return type
    
    * MAE-304 Initial implementation of object selector paging
    
    * MAE-305 Initial implementation of ajax object selector form component using select2
    
    * MAE-304 Add html helper extension method for object selector
    
    * MAE-305 Add styles to object selector
    
    * MAE-304 Improve objects retrieval query and ensure data persistance
    
    * MAE-305 Store selector value in a hidden input
    
    * MAE-304 Move object selector html helper into internal namespace
    
    * MAE-303 Add a switch for identifying objects
    
    * MAE-303 Review changes
    
    * MAE-305 Update select2 package to latest version
    
    * MAE-314 Replace hard-coded values with resource strings
    
    * MAE-303 Adjust .nuspec to include client files
    
    * MAE-312 Add tests for object selector (#125)
    
    * MAE-299 - Fix the GetLinkMetadata route (#117)
    
    * MAE-299 - Fix the GetLinkMetadata route
    
    * MAE-299 Fix typo in comment
    
    Co-authored-by: Ondřej Machala <15248835+OndraM-Kentico@users.noreply.github.com>
    
    * CM-13033 Update webpack to get rid of security vulnerabilities (#122)
    
    * MAE-301 Increment Rich text version to 1.2.1 (#119)
    
    Co-authored-by: Pavel V <pavelv@kentico.com>
    
    * MAE-312 Add tests on object selector html helper and route collection extensions
    
    * MAE-312 Add tests for ObjectsRetriever
    
    * MAE-312 Refactor ObjectsRetriever and add tests for controller
    
    * MAE-312 Fix typos and add some comments
    
    Co-authored-by: Radek Pribyl <radek.pribyl@seznam.cz>
    Co-authored-by: Pavel V <pavelv@kentico.com>
    Co-authored-by: Peter Petrinec <PeterP@kentico.com>
    
    * MAE-321 Add check on virtual context
    
    Co-authored-by: OndraM-Kentico <ondrejm@kentico.com>
    Co-authored-by: Ondřej Machala <15248835+OndraM-Kentico@users.noreply.github.com>
    Co-authored-by: Radek Pribyl <radek.pribyl@seznam.cz>
    Co-authored-by: Pavel V <pavelv@kentico.com>

---
+++ Kentico.Selector.ObjectSelector.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kentico.Selector.ObjectSelector.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kentico.Selector.ObjectSelector.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e39e081e-6e26-4042-a6cc-05e00dcfbd01")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Kentico.Selector.ObjectSelector/ObjectSelectorConstants.cs
@@ -0,0 +1,30 @@
+﻿using Kentico.Components.Web.Mvc.Selectors.Controllers;
+
+namespace Kentico.Components.Web.Mvc.Selectors
+{
+    /// <summary>
+    /// Object selector constants.
+    /// </summary>
+    internal static class ObjectSelectorConstants
+    {
+        /// <summary>
+        /// Name of the controller that serves object selector API calls.
+        /// </summary>
+        public const string CONTROLLER_NAME = "KenticoObjectSelector";
+
+        /// <summary>
+        /// Name of the route that serves object selector data.
+        /// </summary>
+        public const string GET_OBJECTS_ROUTE_NAME = "Kentico.Selector.ObjectSelector.GetObjects";
+
+        /// <summary>
+        /// Route that serves object selector data.
+        /// </summary>
+        public const string GET_OBJECTS_ROUTE = "KenticoComponent/Kentico.Selector.ObjectSelector/" + nameof(KenticoObjectSelectorController.GetObjects);
+
+        /// <summary>
+        /// Name of the event for object selector component initialization.
+        /// </summary>
+        public const string COMPONENT_INITIALIZATION_EVENT_NAME = "Kentico.Selector.ObjectSelector.Initialize";
+    }
+}
