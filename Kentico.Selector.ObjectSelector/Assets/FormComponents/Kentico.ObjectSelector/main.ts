aeca72e62ed0b1f2c1ff82923edd38f8485394b7
Peter Petrinec <PeterP@kentico.com>
2020-05-18 12:59:28 +0200

    MAE-326 Ensure search input and searching message only when needed to reduce flickering on opening the dropdown

--- Kentico.Selector.ObjectSelector/Assets/FormComponents/Kentico.ObjectSelector/main.ts
+++ Kentico.Selector.ObjectSelector/Assets/FormComponents/Kentico.ObjectSelector/main.ts
@@ -10,9 +10,14 @@ const GET_OBJECTS_ENDPOINT_URL_ATTRIBUTE = "data-get-objects-endpoint-url";
 const INITIALIZATION_EVENT_NAME = "Kentico.Selector.ObjectSelector.Initialize";
 const IDENTIFIER_ATTRIBUTE = "data-value-for";
 const IDENTIFY_BY_GUID_ATTRIBUTE = "data-identify-object-by-guid";
+const DROPDOWN_CLASS_NAME = "ktc-object-selector-dropdown";
+const HIDE_SEARCH_CLASS_NAME = "ktc-object-selector-dropdown__search--hide";
+const SEARCHING_MESSAGE_CLASS_NAME = "ktc-object-selector-dropdown--searching";
+const SEARCH_FIELD_CLASS_NAME = "select2-search__field";
 
 document.addEventListener(INITIALIZATION_EVENT_NAME, (event) => {
     const $selector = $(event.target as HTMLSelectElement);
+    let isSearchEnsured = false;
 
     // Remove the name attribute to enforce that the hidden input sends the property value
     $selector.removeAttr("name");
@@ -22,7 +27,7 @@ document.addEventListener(INITIALIZATION_EVENT_NAME, (event) => {
         minimumResultsForSearch: 7,
         // @ts-ignore
         selectionCssClass: "ktc-object-selector-container",  
-        dropdownCssClass: "ktc-object-selector-dropdown",
+        dropdownCssClass: `${DROPDOWN_CLASS_NAME} ${HIDE_SEARCH_CLASS_NAME}`,
         ajax: {
             url: $selector.attr(GET_OBJECTS_ENDPOINT_URL_ATTRIBUTE),
             dataType: 'json',
@@ -42,6 +47,23 @@ document.addEventListener(INITIALIZATION_EVENT_NAME, (event) => {
                     more: data.nextPageAvailable
                 }
             }),
+            transport(params, success, failure) {
+                var $request = $.ajax(params);
+            
+                $request.then((data) => {
+                    if (!isSearchEnsured) {
+                        const $dropdown = $selector.data("select2").$dropdown.find(`.${DROPDOWN_CLASS_NAME}`);
+                        $dropdown.removeClass(HIDE_SEARCH_CLASS_NAME);
+                        $dropdown.find(`input.${SEARCH_FIELD_CLASS_NAME}`).focus();
+                        isSearchEnsured = true;
+                    }
+
+                    return data;
+                }).then(success as JQuery.jqXHR.DoneCallback);
+                $request.fail(failure as JQuery.jqXHR.FailCallback<JQuery.jqXHR>);
+            
+                return $request;
+            },
             cache: true
         },
         language: {
@@ -60,7 +82,21 @@ document.addEventListener(INITIALIZATION_EVENT_NAME, (event) => {
     // Ensure search input placeholder
     $selector.one("select2:open", (event) => {
         const { $dropdown } = $(event.target).data('select2');
-        $dropdown.find("input.select2-search__field").prop("placeholder", getString("SearchPlaceholder"));
+        $dropdown.find(`input.${SEARCH_FIELD_CLASS_NAME}`).prop("placeholder", getString("SearchPlaceholder"));
+    });
+
+    // Ensure "Searching..." message visibility
+    $selector.on("select2:open", (event) => {
+        const { $dropdown } = $(event.target).data('select2');
+        setTimeout(() => {
+            $dropdown.find(`.${DROPDOWN_CLASS_NAME}`).addClass(SEARCHING_MESSAGE_CLASS_NAME);
+        }, 200);
+    });
+
+    // Reset "Searching..." message visibility handling
+    $selector.on("select2:closing", (event) => {
+        const { $dropdown } = $(event.target).data('select2');
+        $dropdown.find(`.${DROPDOWN_CLASS_NAME}`).removeClass(SEARCHING_MESSAGE_CLASS_NAME);
     });
 });
 
