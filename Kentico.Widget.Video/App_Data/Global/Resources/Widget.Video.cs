8f7604fd142ecaf97a4a965665d5858acc8cbcbc
Peter Petrinec <PeterP@kentico.com>
2020-11-02 10:57:09 +0100

    KX-50 Finalize video widget conversion to .net core

---
+++ Kentico.Widget.Video/App_Data/Global/Resources/Widget.Video.cs
@@ -0,0 +1,11 @@
+﻿using Kentico.Content.Web.Mvc;
+using Kentico.Widget.Video.App_Data.Global.Resources.Widget;
+
+[assembly: RegisterPageBuilderLocalizationResource(typeof(Video), "en-us")]
+
+namespace Kentico.Widget.Video.App_Data.Global.Resources.Widget
+{
+    public class Video
+    {
+    }
+}
--- webpack/constants.js
+++ webpack/constants.js
@@ -2,5 +2,5 @@ module.exports = {
   SOURCE_FILES_FOLDER_NAME: "Assets",
   ENTRY_FILE_NAME: "main.*(ts|less)",
   OUTPUT_FOLDER_NAME: "Content",
-  DEV_OUTPUT_PATH: "./SandboxSite/Content",
+  DEV_OUTPUT_PATH: "./SandboxSite/wwwroot/PageBuilder/Public",
 }
